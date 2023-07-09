0a9321ac56379fa6ea26d846d6f9c3dbb9b7f14a
ary <mariwi1503@gmail.com>
2023-07-09 09:04:58 +0700

    test callback

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -6,4 +6,18 @@ router.post("/auth/signup", authController.signup);
 router.post("/auth/login", authController.login);
 router.put("/auth/logout", auth.user, authController.logout);
 
+// for testing xendit callback
+router.post("/callback", async (req, res) => {
+  try {
+    console.log(req.header.toString());
+    res.status(200).json({
+      message: "oke",
+    });
+  } catch (error) {
+    res.status(400).json({
+      error: error.message,
+    });
+  }
+});
+
 module.exports = router;
