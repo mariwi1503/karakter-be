ebdea49dea31e600a52641439027f9ccfe7ff100
ary <mariwi1503@gmail.com>
2023-07-11 23:29:57 +0700

    testing xendit

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -10,7 +10,7 @@ router.put("/auth/logout", auth.user, authController.logout);
 // for testing xendit callback
 router.post("/callback", async (req, res) => {
   try {
-    // console.log(req.header.toString());
+    console.log(req.header.toString());
     const data = axios.post(
       "http://209.97.163.32:4000/api/loan-applications/callback",
       req.body
