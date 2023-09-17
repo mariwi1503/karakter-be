76dfa1aa7598bf34f73bc09eb6e2535cf31d2a4f
ary <mariwi1503@gmail.com>
2023-09-18 06:39:46 +0700

    fix error

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,7 +1,6 @@
 const router = require("express").Router(),
   authController = require("../controllers/authController"),
   auth = require("../middleware/auth");
-(auth = require("../middleware/auth")), (config = require("../config"));
 
 router.post("/auth/signup", authController.signup);
 router.post("/auth/login", authController.login);
